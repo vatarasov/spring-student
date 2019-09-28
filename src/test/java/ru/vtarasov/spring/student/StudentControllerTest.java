762358b05f19e681c8d82827cd30d0ca8032e47d
Vladimir Tarasov <vtarasov@naumen.ru>
2019-09-28 22:02:32 +0500

    Web controller test added

--- src/test/java/ru/vtarasov/spring/student/StudentControllerTest.java
+++ src/test/java/ru/vtarasov/spring/student/StudentControllerTest.java
@@ -1,6 +1,6 @@
 package ru.vtarasov.spring.student;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Optional;
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -8,28 +8,46 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import ru.vtarasov.spring.student.StudentControllerTest.StudentControllerTestConfiguration;
 
 /**
  * @author vtarasov
  * @since 27.09.2019
  */
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(StudentController.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { StudentController.class, StudentControllerTestConfiguration.class })
+@WebAppConfiguration
 public class StudentControllerTest {
-    /*@Autowired
-    private MockMvc mvc;
-
-    @MockBean
-    private StudentRegistrationService studentRegistrationService;
+    @Configuration
+    @EnableWebMvc
+    public static class StudentControllerTestConfiguration {
+        @Bean
+        public StudentRegistrationService studentRegistrationService() {
+            return Mockito.mock(StudentRegistrationService.class);
+        }
+    }
 
     @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private StudentRegistrationService studentRegistrationService;
+
+    private MockMvc mvc;
+
     private ObjectMapper objectMapper;
 
     private Student notRegisteredStudent;
@@ -37,6 +55,10 @@ public class StudentControllerTest {
 
     @Before
     public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+        objectMapper = Jackson2ObjectMapperBuilder.json().build();
+
         notRegisteredStudent = Student.of("Student", 16);
         registeredStudent = notRegisteredStudent.toBuilder().id("id-registered").build();
 
@@ -155,5 +177,5 @@ public class StudentControllerTest {
         mvc
             .perform(MockMvcRequestBuilders.delete("/student/{id}", "id-not-registered"))
             .andExpect(MockMvcResultMatchers.status().isNotFound());
-    }*/
+    }
 }
