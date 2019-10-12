916734a9ed3c38c7a7d37bcecf2a30d7ddc43809
Vladimir Tarasov <vtarasov@naumen.ru>
2019-10-12 18:15:26 +0500

    Logging was added

--- src/main/java/ru/vtarasov/spring/student/StudentRegistrationServiceImpl.java
+++ src/main/java/ru/vtarasov/spring/student/StudentRegistrationServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.vtarasov.spring.student;
 
 import java.util.Optional;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
  * @author vtarasov
  * @since 21.09.2019
  */
+@Log4j2
 @Service
 public class StudentRegistrationServiceImpl implements StudentRegistrationService {
     @Autowired
@@ -15,16 +17,22 @@ public class StudentRegistrationServiceImpl implements StudentRegistrationServic
 
     @Override
     public Student register(Student student) {
-        return repository.save(student);
+        Student saved = repository.save(student);
+        LOG.info("Student was saved. Id: {}, Name: {}, Age: {}", student.getId(), student.getName(), student.getAge());
+        return saved;
     }
 
     @Override
     public void unregister(Student student) {
         repository.deleteById(student.getId());
+        LOG.info("Student was deleted. Id: {}, Name: {}, Age: {}", student.getId(), student.getName(), student.getAge());
     }
 
     @Override
     public Optional<Student> find(String id) {
-        return repository.findById(id);
+        Optional<Student> student = repository.findById(id);
+        student.ifPresent(student1 ->
+            LOG.info("Student was found. Id: {}, Name: {}, Age: {}", student1.getId(), student1.getName(), student1.getAge()));
+        return student;
     }
 }
